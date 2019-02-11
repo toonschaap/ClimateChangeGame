0dfffbaed814d6ccc597bbe38133667ed70bc411
Toon <toonschaap3@gmail.com>
2019-02-11 14:20:58 +0100

    ok

---
+++ ClimateChange/Assets/Scripts/Bow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bow : MonoBehaviour
+{
+    public GameObject projectile;
+    public Transform bulletSpawn;
+    public Animator anim;
+    private float Range = 4f;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //Schiet de pijl
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            
+            ShootAnArrow();
+        }
+
+        
+    }
+
+    void ShootAnArrow()
+    {
+        var bullet = (GameObject)Instantiate(
+            projectile,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        //Snelheid en richting van de pijl
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.right * 40;
+        //Vernietiged de pijl na een bepaalde tijd
+        Destroy(bullet, Range);
+    }
+
+
+}
